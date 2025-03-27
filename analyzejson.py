4fcdd3cd69c6fd9e4679c6fbaabccfd1e986511f
monster-oneday <68390272+monster-oneday@users.noreply.github.com>
2025-03-27 20:37:31 +0800

    Add files via upload

---
+++ analyzejson.py
@@ -0,0 +1,31 @@
+import json
+
+
+filename = 'recipes.json'
+result = dict()
+count = 500
+tmp = 0
+
+with open(filename) as json_file:
+	for line in json_file:
+		recipe = json.loads(line)
+		trigger = recipe['triggerChannelTitle']
+		action = recipe['actionChannelTitle']
+		if trigger in result.keys():
+			if action not in result[trigger]:
+				result[trigger].append(action)
+		else:
+			result[trigger] = [action]
+
+		# if tmp > count:
+		# 	break
+
+		# tmp = tmp + 1
+
+for trigger in result.keys():
+	print trigger.encode('utf-8')
+	print result[trigger]
+	print "------------------------------------------------"
+
+
+		
